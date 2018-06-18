8b19fbe7a041bf0096a38d7aa918d4957cf404dc
Emile Ré <nemile.re@gmail.com>
2018-06-18 14:19:05 +0200

    make gclean a function

--- custom_git.zsh
+++ custom_git.zsh
@@ -1,3 +1,4 @@
 # My custom fonctions
 
-alias gclean='git checkout develop && git pull && git remote prune origin && git branch --merged develop | egrep  -v "(develop|master)$" | xargs git branch -d && git gc'
+alias gcleand='git checkout develop && git pull && git remote prune origin && git branch --merged develop | egrep  -v "(develop|master)$" | xargs git branch -d && git gc'
+alias gcleanm='git checkout master && git pull && git remote prune origin && git branch --merged master | egrep  -v "(develop|master)$" | xargs git branch -d && git gc'
