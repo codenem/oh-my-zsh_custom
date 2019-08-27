b44dff341fa1c97564477995885a4f500ec365ff
Emile Ré <nemile.re@gmail.com>
2019-08-27 16:42:49 +0200

    update cleaning local branches aliases

--- custom_git.zsh
+++ custom_git.zsh
@@ -1,4 +1,4 @@
 # My custom fonctions
 
-alias gcleand='git checkout develop && git pull && comm -12 <(git branch | sed "s/ *//g") <(git remote prune origin | sed "s/^.*origin\///g") | xargs -L1 -J % git branch -D % && git gc'
-alias gcleanm='git checkout master && git pull && comm -12 <(git branch | sed "s/ *//g") <(git remote prune origin | sed "s/^.*origin\///g") | xargs -L1 -J % git branch -D % && git gc'
+alias gcleand='git checkout develop && git pull && git remote prune origin && git fetch --prune && git branch -vv | awk '\''/: gone]/{print $1}'\'' | xargs -L1 -J % git branch -D %'
+alias gcleanm='git checkout master && git pull && git remote prune origin && git fetch --prune && git branch -vv | awk '\''/: gone]/{print $1}'\'' | xargs -L1 -J % git branch -D %'
