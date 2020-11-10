cd773680c70d50e2fb640de622e0e7c566c1b668
Emile Ré <nemile.re@gmail.com>
2020-11-10 14:09:02 +0100

    fix: gclean

--- custom_git.zsh
+++ custom_git.zsh
@@ -1,4 +1,3 @@
 # My custom fonctions
 
-alias gcleand='git checkout develop && git pull && git remote prune origin && git fetch --prune && git branch -vv | awk '\''/: gone]/{print $1}'\'' | xargs -L1 -J % git branch -D %'
-alias gcleanm='git checkout master && git pull && git remote prune origin && git fetch --prune && git branch -vv | awk '\''/: gone]/{print $1}'\'' | xargs -L1 -J % git branch -D %'
+alias gclean='git checkout staging && git pull && git remote prune origin && git fetch --prune && git branch -vv | awk '\''/: gone]/{print $1}'\'' | xargs -L1 -J % git branch -D %'
